7ffd1d3b85ea1732e648a16bc07f83ef18fa5a29
sidon59 <xmakhubele83@gmail.com>
2024-11-13 14:52:26 +0200

    server.js comments

--- public/server.js
+++ public/server.js
@@ -4,13 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
+// Serve "public" directory
 app.use(express.static('public'));
 
 app.get('/',(res,req)=>{
      res.render('index');
  });
-// Start the server
+// Starting the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
