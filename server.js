33a3dd7253ccd0a4146e30e4ec87a030b6d5a917
sidon59 <xmakhubele83@gmail.com>
2024-11-15 09:48:45 +0200

    uploading of node modules and changing a port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Serve directory
 app.use(express.static(__dirname));
